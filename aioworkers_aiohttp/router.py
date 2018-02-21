b1f8362df2241a88a6f7d45fd307f53151deef4b
Alexander Malev <malev@somedev.ru>
2018-02-21 11:16:28 +0300

    AbstractSwaggerRouter

--- aioworkers_aiohttp/router.py
+++ aioworkers_aiohttp/router.py
@@ -3,8 +3,10 @@ from pathlib import Path
 from aiohttp_apiset import SwaggerRouter
 from aioworkers.utils import import_name
 
+from .abc import AbstractSwaggerRouter
 
-class Router(SwaggerRouter):
+
+class Router(SwaggerRouter, AbstractSwaggerRouter):
     def __init__(self, search_in_modules=(), search_dirs=(), **kwargs):
         modules = map(import_name, search_in_modules)
         dirs = [Path(x.__file__).parent for x in modules]
