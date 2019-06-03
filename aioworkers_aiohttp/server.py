8c1f446d0e667527eb4b4353306063bd5cb4de0e
Alexander Malev <yttrium@somedev.ru>
2019-06-03 18:58:26 +0300

    fix WebServer

--- aioworkers_aiohttp/server.py
+++ aioworkers_aiohttp/server.py
@@ -6,23 +6,17 @@ class WebServer(SocketServer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._runner = None
-        self._site = None
 
     async def init(self):
+        groups = self.config.get('groups', ())
+        self.context.on_start.append(self.start, groups)
+        self.context.on_stop.append(self.stop, groups)
+
+    async def start(self):
         self._runner = web.AppRunner(self.context.app)
         await self._runner.setup()
-        self.context.on_connect.append(
-            self.connect,
-            self.config.get('groups', ())
-        )
-        self.context.on_disconnect.append(
-            self.disconnect,
-            self.config.get('groups', ())
-        )
-
-    async def connect(self):
         for sock in self._sockets:
             await web.SockSite(self._runner, sock).start()
 
-    async def disconnect(self, ):
+    async def stop(self):
         await self._runner.cleanup()
