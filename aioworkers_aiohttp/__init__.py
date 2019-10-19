51e63d9c952635f9015235bf0eed6e72fcd341a1
Alexander Malev <yttrium@somedev.ru>
2019-10-20 01:50:19 +0300

    plugin interface

--- aioworkers_aiohttp/__init__.py
+++ aioworkers_aiohttp/__init__.py
@@ -1,4 +1,40 @@
+import argparse
+from pathlib import Path
+from typing import Dict, Iterable, Tuple
+
+from aioworkers.core.plugin import Plugin
+
+
 try:
     from .version import __version__
 except ImportError:
     __version__ = 'dev'
+
+BASE = Path(__file__).parent
+
+
+class plugin(Plugin):
+    configs = (
+        BASE / 'plugin.ini',
+    )
+
+    def __init__(self):
+        super().__init__()
+        self._config = {}
+
+    def get_config(self) -> Dict:
+        return self._config
+
+    def add_arguments(self, parser: argparse.ArgumentParser):
+        try:
+            parser.add_argument('--port', type=int)
+        except argparse.ArgumentError:
+            pass
+
+    def parse_known_args(
+        self, args: Iterable[str],
+        namespace: argparse.Namespace,
+    ) -> Tuple[argparse.Namespace, Iterable[str]]:
+        if namespace.port:
+            self._config['http.port'] = namespace.port
+        return namespace, args
