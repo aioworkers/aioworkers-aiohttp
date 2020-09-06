09bc5c23277e867125a9e462151e7c74131f8e00
Alexander Malev <yttrium@somedev.ru>
2020-09-07 02:55:10 +0300

    fix example

--- example/access_log_class.py
+++ example/access_log_class.py
@@ -7,10 +7,10 @@ from aiohttp.abc import AbstractAccessLogger
 
 class DebugAccessLogClass(AbstractAccessLogger):
     def log(self, request, response, time):
+        data = ''
         with suppress(JSONDecodeError, UnicodeDecodeError):
             data = response.body.decode()
             data = json.dumps(json.loads(data), indent=2, ensure_ascii=False)
-        data = data or ''
         self.logger.info(
             f'{request.remote} {request.method} {request.path} - {response.status}\n'
             f'{data[:100]}\n'
